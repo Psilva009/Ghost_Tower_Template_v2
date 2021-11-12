0953cbf94f7e05532ec5ac95b4a89e56faf2c298
Psilva009 <56855496+Psilva009@users.noreply.github.com>
2021-11-12 15:31:16 -0300

    docs: Base

--- sketch.js
+++ sketch.js
@@ -1,11 +1,28 @@
+var imagemDaTorre, torre;
+var porta, grupoDePortas;
+var escalador;
+var fantasma, imagemDoFantasma;
+var blocoInvisivel, grupoDeBlocosInvisiveis;
+var som;
+var pontuacao;
 
 function preload(){
-
+  imagemDoFantasma = loadImage("ghost-standing.png");
+  imagemDaTorre = loadImage("tower.png");
 }
 
 function setup(){
   createCanvas(600,600);    
+  pontuacao = 0;
 
+  torre = createSprite(300,300);
+  torre.addImage("tower",imagemDaTorre);
+  
+  grupoDePortas = new Group();
+
+  fantasma = createSprite(300, 300, 50, 50);
+  fantasma.scale = 0.3;
+  fantasma.addImage("ghost", imagemDoFantasma);
 }
 
 
@@ -13,6 +30,20 @@ function draw(){
   background('black');
   drawSprites();
   
+  fill('white');
+  text("Pontuação: "+pontuacao, 500, 50);
+  pontuacao = pontuacao + Math.round(getFrameRate()/ 60);
+
+  gerarPortas();
 }
 
+function gerarPortas(){
+  if(frameCount % 300 == 0){
+    porta = createSprite(200,-50);
+    porta.velocityY = 2;
 
+    porta.lifetime = 420;
+
+    grupoDePortas.add(porta);
+  }
+}
