c9e442a29edc31ba56323011dbc3932486a96649
marip <mariana.silva@solucx.com.br>
2021-11-30 15:01:43 -0300

    docs: Adicionando movimentação e estados

--- sketch.js
+++ sketch.js
@@ -5,10 +5,15 @@ var fantasma, imagemDoFantasma;
 var blocoInvisivel, grupoDeBlocosInvisiveis;
 var som;
 var pontuacao;
+const INICIO = 0;
+const JOGANDO = 1;
+const FIM = 2;
+var estado = INICIO;
 
 function preload(){
   imagemDoFantasma = loadImage("ghost-standing.png");
   imagemDaTorre = loadImage("tower.png");
+  imagemDaPorta = loadImage("door.png");
 }
 
 function setup(){
@@ -29,19 +34,53 @@ function setup(){
 function draw(){
   background('black');
   drawSprites();
-  
   fill('white');
   text("Pontuação: "+pontuacao, 500, 50);
-  pontuacao = pontuacao + Math.round(getFrameRate()/ 60);
+  if(estado == INICIO && keyDown('space')){
+    estado = JOGANDO;
+  }
+  if (estado == INICIO){
+    torre.velocityY = 0;
+    fill('red');
+    textSize(20);
+    text("Aperte espaço para iniciar o jogo", 150, 200);
+  }
+  if (estado == JOGANDO){
+    pontuacao = pontuacao + Math.round(getFrameRate()/ 60);
+    torre.velocityY = 2;
+    if(torre.y > 400){
+      torre.y = 300
+    }
+    
+    if(keyDown("left_arrow")){
+      fantasma.x = fantasma.x - 3;
+    }
 
-  gerarPortas();
+    if(keyDown("right_arrow")){
+      fantasma.x = fantasma.x + 3;
+    }
+
+    if(keyDown("space")){
+      fantasma.velocityY = -10;
+    }
+    
+    fantasma.velocityY = fantasma.velocityY + 0.8;
+    gerarPortas();
+  }
+  if (estado == FIM){
+    stroke("yellow");
+    fill("yellow");
+    textSize(30);
+    text("Game Over", 220,300);
+  }
 }
 
 function gerarPortas(){
   if(frameCount % 300 == 0){
     porta = createSprite(200,-50);
     porta.velocityY = 2;
-
+    porta.addImage(imagemDaPorta);
+    
     porta.lifetime = 420;
 
     grupoDePortas.add(porta);
