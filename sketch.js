6a5f65cbdb110cb2252cb9b84270c301ce05652e
marip <mariana.silva@solucx.com.br>
2022-01-18 19:59:19 -0300

    docs: Adicionando comentários para facilitar

--- sketch.js
+++ sketch.js
@@ -1,19 +1,23 @@
 var imagemDaTorre, torre;
-var porta, grupoDePortas;
-var escalador;
-var fantasma, imagemDoFantasma;
+var porta;
+var escalador, imagemDoEscalador, grupoDeEscaladores;
+var fantasma;
 var blocoInvisivel, grupoDeBlocosInvisiveis;
 var som;
 var pontuacao;
 const INICIO = 0;
 const JOGANDO = 1;
 const FIM = 2;
+
 var estado = INICIO;
 
-function preload(){
+function preload() {
+  // carregar som
   imagemDoFantasma = loadImage("ghost-standing.png");
   imagemDaTorre = loadImage("tower.png");
-  imagemDaPorta = loadImage("door.png");
+  imagemDoEscalador = loadImage("climber.png");
+  // carregar imagem do fantasma
+  
 }
 
 function setup(){
@@ -23,11 +27,10 @@ function setup(){
   torre = createSprite(300,300);
   torre.addImage("tower",imagemDaTorre);
   
-  grupoDePortas = new Group();
+  grupoDeEscaladores = new Group();
+  grupoDeBlocosInvisiveis = new Group();
 
   fantasma = createSprite(300, 300, 50, 50);
-  fantasma.scale = 0.3;
-  fantasma.addImage("ghost", imagemDoFantasma);
 }
 
 
@@ -36,9 +39,7 @@ function draw(){
   drawSprites();
   fill('white');
   text("Pontuação: "+pontuacao, 500, 50);
-  if(estado == INICIO && keyDown('space')){
-    estado = JOGANDO;
-  }
+  // criar if para iniciar o jogo
   if (estado == INICIO){
     torre.velocityY = 0;
     fill('red');
@@ -47,7 +48,7 @@ function draw(){
   }
   if (estado == JOGANDO){
     pontuacao = pontuacao + Math.round(getFrameRate()/ 60);
-    torre.velocityY = 2;
+    // aumentar velocidade da torre
     if(torre.y > 400){
       torre.y = 300
     }
@@ -57,7 +58,7 @@ function draw(){
     }
 
     if(keyDown("right_arrow")){
-      fantasma.x = fantasma.x + 3;
+      // movimentar fantasma para direita
     }
 
     if(keyDown("space")){
@@ -66,6 +67,17 @@ function draw(){
     
     fantasma.velocityY = fantasma.velocityY + 0.8;
     gerarPortas();
+
+    // if para deixar o fantasma parado na porta
+
+    if(grupoDeBlocosInvisiveis.isTouching(fantasma) || fantasma.y > 600){
+      fantasma.destroy();
+      torre.destroy();
+      grupoDeEscaladores.destroyEach();
+      grupoDeBlocosInvisiveis.destroyEach();
+      // excluir porta
+      estado = FIM;
+    }
   }
   if (estado == FIM){
     stroke("yellow");
@@ -78,11 +90,32 @@ function draw(){
 function gerarPortas(){
   if(frameCount % 300 == 0){
     porta = createSprite(200,-50);
-    porta.velocityY = 2;
     porta.addImage(imagemDaPorta);
     
+    escalador = createSprite(200,10);
+    escalador.addImage(imagemDoEscalador);
+    
+    blocoInvisivel = createSprite(200,25);
+    blocoInvisivel.width = escalador.width;
+    blocoInvisivel.height = 2;
+    blocoInvisivel.visible = false;
+    
+    // gerar posição aleatória para a porta
+    escalador.x = porta.x;
+    blocoInvisivel.x = porta.x;
+    
+    escalador.velocityY = 2;
+    porta.velocityY = 2;
+    blocoInvisivel.velocityY = 2;
+    
     porta.lifetime = 420;
-
-    grupoDePortas.add(porta);
+    escalador.lifetime = 420;
+    
+    fantasma.depth = porta.depth;
+    fantasma.depth +=1;
+    
+    // adicionar porta ao grupo
+    grupoDeEscaladores.add(escalador);
+    grupoDeBlocosInvisiveis.add(blocoInvisivel);
   }
 }
